f28d25f5f0318382e31919b587495c1a4cf95ad1
Aliaksandr <a.kuchur@godeltech.com>
2023-03-01 01:20:54 +0300

    feat(task-2): Task 2 implementation (form validation) Added validation for the form for `name` field (Task 2)

--- src/components/ui/forms/FccNameSearchForm.vue
+++ src/components/ui/forms/FccNameSearchForm.vue
@@ -6,6 +6,7 @@ import {
   ElFormItem,
   ElInput,
   FormInstance,
+  FormItemRule,
 } from "element-plus";
 
 type FormModelValue = {
@@ -21,22 +22,51 @@ const emit = defineEmits<{
 
 const formRef = ref<FormInstance>();
 
+const rules = computed<Record<keyof FormModelValue, FormItemRule[]>>(() => ({
+  name: [
+    {
+      required: true,
+      message: "The field value is required",
+    },
+    {
+      pattern: new RegExp(/^[a-zA-Z]+$/),
+      message: "The field value must contain only letters",
+    },
+    {
+      min: 2,
+      message: "The length of the name should be at least 2 letters",
+    },
+  ],
+}));
+
 const name = computed({
   get: () => props.name,
   set: (newName) => {
     emit("update:name", newName);
   },
 });
+
+async function onFormSubmit(formInstance: FormInstance | undefined) {
+  if (!formInstance) {
+    return;
+  }
+
+  const isValid = await formInstance.validate();
+
+  if (isValid) {
+    emit("submit");
+  }
+}
 </script>
 
 <template>
-  <ElForm ref="formRef" :model="props" label-width="50px">
+  <ElForm ref="formRef" :model="props" :rules="rules" label-width="50px">
     <ElFormItem label="Name" prop="name">
       <ElInput v-model="name" />
     </ElFormItem>
 
     <ElFormItem>
-      <ElButton type="primary" @click="emit('submit')">Submit</ElButton>
+      <ElButton type="primary" @click="onFormSubmit(formRef)">Submit</ElButton>
     </ElFormItem>
   </ElForm>
 </template>
