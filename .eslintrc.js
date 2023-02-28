1537d8f22e6d43fbc958e44353c2d5b9aa26ef54
Aliaksandr <a.kuchur@godeltech.com>
2023-03-01 01:08:23 +0300

    chore: TypeScript Linter rules lower strictness TS Linter rules were decreased in strictness

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "@vue/typescript/recommended",
+    "@vue/typescript",
     "plugin:prettier/recommended",
   ],
   parserOptions: {
